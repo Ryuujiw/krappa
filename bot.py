ae1d79d97baf209e8ca1dd6637ce9a846beaaf73
Ryuujiw <rujinwong@gmail.com>
2021-02-08 16:42:49 +0800

    fixed incorrect results due to limit and now able to ask for different emote stats

--- bot.py
+++ bot.py
@@ -18,30 +18,34 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user or not message.content.startswith('krappa who'):
+
+    emote_to_check = ''
+
+    if message.content.endswith('who'):
+        # get the emote
+        emote_to_check = message.content.split()[0].lower()
+
+    if emote_to_check == '':
         return
 
     # init dataframe
-    data = pd.DataFrame(columns=['author', 'reactions', 'time'])
+    data = pd.DataFrame(columns=['author', 'message', 'reactions', 'time'])
     
     # get 1st day of current month -> today
     today = datetime.datetime.today()
     start_of_the_month = datetime.datetime.today().replace(day=1)
-
-    async for msg in message.channel.history(before=today, after=start_of_the_month):
+    async for msg in message.channel.history(before=today, after=start_of_the_month, limit=None):
         if msg.author != client.user:
             # skip message if has no reactions
             if len(msg.reactions) == 0:
                 continue
             for emote in msg.reactions:
-                if not hasattr(emote.emoji, 'name') or not emote.emoji.name == 'Krappa':
-                    continue
-
-                data = data.append({'author': msg.author.name,
-                                    'message': msg.content,
-                                    'reactions' : emote.count,
-                                    'time': msg.created_at},
-                                    ignore_index=True)
+                if hasattr(emote.emoji, 'name') and emote.emoji.name.lower() == emote_to_check:
+                    data = data.append({'author': msg.author.name,
+                    'message': msg.content,
+                    'reactions' : emote.count,
+                    'time': msg.created_at},
+                    ignore_index=True)
 
     # find most krappa
     krappa = data['reactions']
@@ -51,13 +55,13 @@ async def on_message(message):
     winners = nominees.to_dict('records')
 
     # init embed
-    embed = discord.Embed(title="Krappa of the month", color=0x00ff00)
+    embed = discord.Embed(title= emote_to_check + " of the month", color=0x00ff00)
     embed.set_thumbnail(url="https://cdn.betterttv.net/emote/58cd3345994bb43c8d300b82/3x")
 
     for winner in winners:
         embed.add_field(name="Author", value=winner['author'])
         embed.add_field(name="Message", value=winner['message'])
-        embed.add_field(name="Krappa'd", value=winner['reactions'])
+        embed.add_field(name= emote_to_check + "'d", value=winner['reactions'])
         embed.add_field(name="Time", value=winner['time'])
         embed.add_field(name = chr(173), value = chr(173))
         embed.add_field(name = chr(173), value = chr(173))
