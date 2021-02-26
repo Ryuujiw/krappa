d915c634e63cdb596855170b7caabbf71cc32ef0
wjkhoo96 <wjkhoo@yahoo.com>
2021-02-27 00:24:42 +0800

    generate and embed podium for winners

--- bot.py
+++ bot.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 import asyncio
 import pandas as pd
 import datetime
+from PIL import Image
+import requests
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -45,7 +47,8 @@ async def on_message(message):
                     'message': msg.content,
                     'message_url': msg.jump_url,
                     'reactions' : emote.count,
-                    'time': msg.created_at},
+                    'time': msg.created_at,
+                    'avatar_url': msg.author.avatar_url},
                     ignore_index=True)
 
     # find most krappa
@@ -55,18 +58,33 @@ async def on_message(message):
     # store winners into a csv
     winners = nominees.to_dict('records')
 
+    # init winners' podium
+    podium = Image.open("tmp/podium.png")
+    size = (80,80)        
+
     # init embed
     embed = discord.Embed(title= emote_to_check + " of the month", color=0x00ff00)
-    embed.set_thumbnail(url="https://cdn.betterttv.net/emote/58cd3345994bb43c8d300b82/3x")
 
-    for winner in winners:
+    for index, winner in enumerate(winners):
         embed.add_field(name="Author", value=winner['author'])
         embed.add_field(name="Message", value='[%s](%s)'%(winner['message'],winner['message_url']))
         embed.add_field(name= emote_to_check + "'d", value=winner['reactions'])
         embed.add_field(name="Time", value=winner['time'])
         embed.add_field(name = chr(173), value = chr(173))
         embed.add_field(name = chr(173), value = chr(173))
+
+        # create winners' podium
+        winner = Image.open(requests.get(winner['avatar_url'], stream=True).raw)
+        resized_avatar = winner.resize(size)
+        # first, second and runner-up
+        podium.paste(resized_avatar,(375,40))
+        podium.paste(resized_avatar,(120,110))  if index == 1 else None
+        podium.paste(resized_avatar,(605,50))   if index == 2 else None
+        podium.save('tmp/winners.png')
+
+    file = discord.File("tmp/winners.png", filename="image.png") if len(winners) > 0 else None
+    embed.set_image(url="attachment://image.png")
         
-    await message.channel.send(embed=embed)
+    await message.channel.send(file=file, embed=embed)
 
 client.run(TOKEN)
